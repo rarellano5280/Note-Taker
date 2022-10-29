19f18dd69c0fde28fe0b652078fc474e6628f66e
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-10-29 17:44:27 -0600

    cloned starter code and added server.js file with dependincies and express

---
+++ server.js
@@ -0,0 +1,10 @@
+//Depend
+const express = require('express');
+const fs = require('fs');
+const path = require('path')
+
+//Express set up. 
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
